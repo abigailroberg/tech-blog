06e310dbce0f68d623b976e519169ab6b9c1c490
Abby Roberg <abbyroberg@gmail.com>
2021-09-27 12:18:53 -0500

    comment model

---
+++ models/Comment.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize')
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+)
