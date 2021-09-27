b9e93eb2a986022f316f5b2df2a6c2cffc5fb0f2
Abby Roberg <abbyroberg@gmail.com>
2021-09-27 14:08:27 -0500

    remaining user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -48,4 +48,39 @@ router.get('/:id', (req, res) => {
     })
 })
 
+// create new user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+// delete user by id
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found wit this id' })
+            return
+        }
+        res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
 module.exports = router
