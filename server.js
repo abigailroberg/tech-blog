4473f8f87fac9a73d95a20629eebff4d1af1ab19
Abby Roberg <abbyroberg@gmail.com>
2021-09-27 16:22:06 -0500

    set up handlebars for homepage

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const controllers = require('./controllers')
 const sequelize = require('./config/connection')
+const path = require('path')
 
+// set up handlebars
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({})
+
+// set up app
 const app = express()
 const PORT = process.env.PORT || 3001
 
+// middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// handlebars as default
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 // turn on controllers
 app.use(controllers)
