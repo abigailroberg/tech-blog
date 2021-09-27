98b7942c649955eb8a8831623bdcd39d7d9e5b76
Abby Roberg <abbyroberg@gmail.com>
2021-09-27 12:14:16 -0500

    post model

---
+++ models/Post.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize')
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        text_content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
