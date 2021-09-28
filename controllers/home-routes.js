18bde83484dcf86b55eea11a0f8d16ee829b8f1a
Abby Roberg <abbyroberg@gmail.com>
2021-09-28 11:50:08 -0500

    homepage styling and handlebars template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.render('homepage')
+    res.render('homepage', {
+        id: 1,
+        title: 'Post Title to Format Homepage',
+        text_content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi provident et molestiae repudiandae labore sunt iste unde dolore magnam, placeat suscipit consequuntur atque! Voluptas quasi dolorum magnam quo rerum temporibus.',
+        created_at: new Date(),
+        user: {
+            username: 'test user'
+        }
+    })
 })
 
 module.exports = router
