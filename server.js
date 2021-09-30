db10a0dddfe75ce9161e3eac918d2aee7c2b02c8
Abby Roberg <abbyroberg@gmail.com>
2021-09-30 11:34:33 -0500

    enabled sessions

--- server.js
+++ server.js
@@ -1,24 +1,46 @@
 const express = require('express')
-const controllers = require('./controllers')
-const sequelize = require('./config/connection')
-const path = require('path')
-
-// set up handlebars
 const exphbs = require('express-handlebars')
-const hbs = exphbs.create({})
+const session = require('express-session')
+const path = require('path')
 
 // set up app
 const app = express()
 const PORT = process.env.PORT || 3001
+
+// set up sequelize with session
+const sequelize = require('./config/connection')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const sess = {
+  secret: 'Secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
+
+app.use(session(sess))
+
+// get helper function(s)
+const helpers = require('./utils/helpers')
+
+// create handlebars with access to helpers
+const hbs = exphbs.create({ helpers })
+
+// handlebars as default
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+const controllers = require('./controllers')
 
 // middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// handlebars as default
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
+
 
 // turn on controllers
 app.use(controllers)
