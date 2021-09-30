47492245a84f06637a560cd6c1ee771cacc4df72
Abby Roberg <abbyroberg@gmail.com>
2021-09-30 11:02:07 -0500

    signup front end

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,18 @@
+// login function
 async function loginFormHandler(event) {
     event.preventDefault()
     
     // get username and password from form entry
-    const user = document.querySelector('#username-login').value.trim()
-    const pw = document.querySelector('#password-login').value.trim()
+    const username = document.querySelector('#username-login').value.trim()
+    const password = document.querySelector('#password-login').value.trim()
 
     // check for username and password entered
-    if(user && pw) {
+    if(username && password) {
         const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
-                user,
-                pw
+                username,
+                password
             }),
             headers: ({ 'Content-Type': 'application/json' })
         })
@@ -22,4 +23,32 @@ async function loginFormHandler(event) {
     }
 }
 
+// signup function
+async function signupFormHandler(event) {
+    event.preventDefault()
+
+    // get username and password from entry
+    const username = document.querySelector('#username-signup').value.trim()
+    const password = document.querySelector('#password-signup').value.trim()
+
+    // create new account with username and password
+    if(username && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+        if(response.ok) {
+            document.location.replace('/')
+        } else {
+            alert(response.statusText)
+        }
+    }
+}
+
 document.querySelector('#login-form').addEventListener('submit', loginFormHandler)
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler)
