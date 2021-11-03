d1c670c9f47d46e8b74fa2cdff39f84d509ae597
Abigail Roberg <abbyroberg@gmail.com>
2021-11-03 11:02:56 -0500

    dashboard route and handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,7 +4,43 @@ const { User, Post, Comment } = require('../models')
 
 // render dashboard
 router.get('/', (req, res) => {
-    res.render('dashboard')
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'text_content',
+            'created_at'
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }))
+        res.render('dashboard', {
+            posts,
+            loggedIn: req.session.loggedIn
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
 })
 
 module.exports = router
