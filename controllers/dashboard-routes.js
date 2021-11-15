8b1f52e37545b807784154f476c5177940f1adc1
Abigail Roberg <abbyroberg@gmail.com>
2021-11-15 15:35:47 -0600

    rearranged dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -44,4 +44,53 @@ router.get('/:id', (req, res) => {
     })
 })
 
+// edit post display
+router.get('/post/edit/:id', (req, res) => {
+    // get single post including comments
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'text_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id','comment_text','created_at','user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with that id' })
+            return
+        }
+        const post = dbPostData.get({ plain: true })
+
+        console.log(post)
+
+        res.render('edit-post', {
+            post,
+            loggedIn: req.session.loggedIn,
+            id: req.session.user_id
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
 module.exports = router
--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -18,7 +18,7 @@ async function editPost(event) {
     })
     
     if (response.ok) {
-        document.location.replace('/dashboard/')
+        document.location.reload()
     } else {
         alert(response.statusText)
     }
